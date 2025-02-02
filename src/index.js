143ff771a053dac2e01d0997a80117a02ec56811
btousifar <bn.tousifar86@gmail.com>
2025-02-02 11:24:56 -0600

    Set up class Todo and toDoList module
    used class Todo and set the properties to
    generate an object. Used toDoList as a factory
    function to handle add, delete the toDoListItems
    array
    
    [Ticket: 1]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import "./styles.css";
+
+class Todo {
+    constructor(title, description = '', dueDate = 'No due date', priority = 'Low') {
+        this.id = Date.now();
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+
+
+
+function toDoList() {
+    const toDoListItems = [];
+
+    function addTodo(item) {
+        toDoListItems.push(item);
+    }
+
+    function getTodo() {
+        return toDoListItems;
+    }
+
+    function selectTodo(index) {
+        return toDoListItems[index]
+    }
+
+    function deleteTodo(index) {
+        toDoListItems.splice(index, 1);
+    }
+
+
+    return {addTodo, getTodo, selectTodo, deleteTodo}
+}
+
+
+const list = toDoList();
+list.addTodo(new Todo('laundry', "parisa's cloths", 'Wednesday', 'Medium'));
+list.addTodo(new Todo('laundry', "parisa's cloths", 'Wednesday', 'Medium'));
+list.addTodo(new Todo('shopping'))
+// list.deleteTodo(1)
+
+
+console.log(list.getTodo());
+console.log(list.selectTodo(0))
+
+
